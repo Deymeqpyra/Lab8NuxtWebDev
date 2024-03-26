85cd89094a0892c23e6787094a9c1b342052c0c9
Deymeqpyra <deymecop@gmail.com>
2024-03-26 16:10:13 +0200

    Feat: Add modules and folder pages, which contains all pages

---
+++ nuxt-app/pages/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+import Studentlist from "~/pages/studentlist.vue";
+</script>
+
+<template>
+  <button>
+  <studentlist></studentlist>
+  </button>
+</template>
+
+<style scoped>
+
+</style>
---
+++ nuxt-app/pages/studentlist.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <h1>Hello students</h1>
+</template>
+
+<style scoped>
+
+</style>

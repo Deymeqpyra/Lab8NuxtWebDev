9208fa7052d23dcac2d94825adcab211bb15072f
Deymeqpyra <deymecop@gmail.com>
2024-04-02 03:09:50 +0300

    Feat: Add sortable for columns.

--- nuxt-app/pages/studentlist.vue
+++ nuxt-app/pages/studentlist.vue
@@ -1,85 +1,126 @@
-  <script setup lang="ts">
-  // import { ref, computed } from 'vue';
-  const columns = [
-    {
+<script setup lang="ts">
+// import { ref, computed } from 'vue';
+
+
+
+const columns = [
+  {
     key: 'title',
-    label: 'Title'
+    label: 'Title',
+    sortable: true
   }, {
     key: 'description',
-    label: 'Description'
+    label: 'Description',
+    sortable: true
   }, {
     key: 'price',
-    label: 'Price'
+    label: 'Price',
+    sortable: true
   }, {
-      key: 'rating',
-      label: 'Rating',
-    }, {
+    key: 'rating',
+    label: 'Rating',
+    sortable: true
+  }, {
     key: 'brand',
-    label: 'Brand'
+    label: 'Brand',
+    sortable: true
   },
-    {
-      key: 'category',
-      label: 'Category'
-    },
-    {
-      key: 'thumbnail',
-      label: 'Thumbnail'
-    }];
+  {
+    key: 'category',
+    label: 'Category',
+    sortable: true
+  },
+  {
+    key: 'thumbnail',
+    label: 'Thumbnail'
+  }];
 
+const sort = ref({ column: 'title', direction: 'asc' as const })
+const sortedRows = computed(() => {
+  const sortedProducts = [...productslist]
+  const { column, direction } = sort.value
 
+  if (column && direction) {
+    sortedProducts.sort((a, b) => {
+      const aValue = a[column]
+      const bValue = b[column]
+      if (aValue < bValue) return direction === 'asc' ? -1 : 1
+      if (aValue > bValue) return direction === 'asc' ? 1 : -1
+      return 0
+    })
+  }
 
-  const { data } = await useFetch<any>('https://dummyjson.com/products');
-  let productslist = data.value.products;
+  return sortedProducts
+});
 
-  const q = ref('');
-  const page = ref(1);
-  const pageCount = 5;
+const { data } = await useFetch<any>('https://dummyjson.com/products');
+let productslist = data.value.products;
 
-  const filteredData = computed(() => {
-    if (!q.value) {
-      return productslist;
-    }
+const q = ref('');
+const page = ref(1);
+const pageCount = 5;
 
-    return productslist.filter((products) => {
-      return Object.values(products).some((value) => {
-        return String(value).toLowerCase().includes(q.value.toLowerCase());
-      });
+const filteredData = computed(() => {
+  if (!q.value) {
+    return productslist;
+  }
+
+  return productslist.filter((products) => {
+    return Object.values(products).some((value) => {
+      return String(value).toLowerCase().includes(q.value.toLowerCase());
     });
   });
+});
 
-  const rows = computed(() => {
-    return filteredData.value.slice((page.value - 1) * pageCount, page.value * pageCount);
-  });
-  watch(q,() => {
-    page.value = 1;
-  });
-  </script>
-  <template>
-    <title>Student List</title>
-    <div>
-      <div class="flex px-3 py-3.5 border-b border-gray-200 dark:border-gray-700">
-        <UInput v-model="q" placeholder="Filter people..." />
-      </div>
-      <UTable :rows="rows" :columns="columns">
-        <template #thumbnail-data="{row}">
-          <img :src="row.thumbnail" alt="" class="w-[100px] h-[100px]">
-        </template>
-        <template #description-data="{row}">
-          <div class="text-wrap">{{ row.description }}</div>
-        </template>
-        <!-- Rating cell -->
-        <template #rating-data="{row}">
-          <div :class="{ 'bg-red-500/40': row.rating < 4.5, 'bg-green-500/40': row.rating >= 4.5 }" class="px-3 py-2">
-            {{ row.rating }}
-          </div>
-        </template>
 
-      </UTable>
-      <div class="flex justify-center px-3 py-3.5 border-t border-gray-200 dark:border-gray-700">
-        <UPagination v-model="page" :page-count="pageCount" :total="productslist.length" />
-      </div>
+
+const rows = computed(() => {
+  let filteredProducts = [...sortedRows.value]
+
+  if (q.value) {
+    filteredProducts = filteredProducts.filter(product => {
+      return Object.values(product).some(value => {
+        return String(value).toLowerCase().includes(q.value.toLowerCase())
+      })
+    })
+  }
+
+  const startIndex = (page.value - 1) * pageCount
+  const endIndex = startIndex + pageCount
+
+  return filteredProducts.slice(startIndex, endIndex)
+})
+watch(q,() => {
+  page.value = 1;
+});
+</script>
+<template>
+  <title>Student List</title>
+  <div>
+    <div class="flex px-3 py-3.5 border-b border-gray-200 dark:border-gray-700">
+      <UInput v-model="q" placeholder="Filter people..." />
     </div>
-  </template>
+    <UTable :rows="rows" :columns="columns" v-model:sort="sort"
+    sort-mode="manual">
+      <template #thumbnail-data="{row}">
+        <img :src="row.thumbnail" alt="" class="w-[100px] h-[100px]">
+      </template>
+      <template #description-data="{row}">
+        <div class="text-wrap">{{ row.description }}</div>
+      </template>
+      <!-- Rating cell -->
+      <template #rating-data="{row}">
+        <div :class="{ 'bg-red-500/40': row.rating < 4.5, 'bg-green-500/40': row.rating >= 4.5 }" class="px-3 py-2">
+          {{ row.rating }}
+        </div>
+      </template>
 
-  <style>
-  </style>
+    </UTable>
+    <div class="flex justify-center px-3 py-3.5 border-t border-gray-200 dark:border-gray-700">
+      <UPagination v-model="page" :page-count="pageCount" :total="productslist.length" />
+    </div>
+  </div>
+</template>
+
+<style>
+</style>
